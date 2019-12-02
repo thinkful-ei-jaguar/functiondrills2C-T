32071575eea0335e45c7d88ef8e6767c255bdb70
Taylor Bradshaw <taylordyoung1@gmail.com>
2019-12-02 18:59:55 -0500

    created decode function and decoded message

--- functiondrills2.js
+++ functiondrills2.js
@@ -22,3 +22,30 @@ console.log(beyond(0));
 console.log(beyond(-5));
 console.log(beyond(10));
 console.log(beyond(Infinity));
+
+function decode (word) {
+    if (word[0] === "a") {
+        return word[1];
+    }
+    if (word[0] === "b") {
+        return word[2];
+    }
+    if (word[0] === "c") {
+        return word[3];
+    }
+    if (word[0] === "d") {
+        return word[4];
+    }
+    else {
+        return " ";
+    }
+}
+
+console.log(decode('craft'));
+console.log(decode('block'));
+console.log(decode('argon'));
+console.log(decode('meter'));
+console.log(decode('bells'));
+console.log(decode('brown'));
+console.log(decode('croon'));
+console.log(decode('droop'));
